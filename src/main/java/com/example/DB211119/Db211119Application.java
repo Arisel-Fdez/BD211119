bfd482baccca40d2ac8937ed4192426f6aceee1f
Arisel Fernandez Cañaveral <84791183+Arisel-Fdez@users.noreply.github.com>
2022-03-01 08:47:02 -0600

    Add files via upload

---
+++ src/main/java/com/example/DB211119/Db211119Application.java
@@ -0,0 +1,13 @@
+package com.example.DB211119;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db211119Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Db211119Application.class, args);
+	}
+
+}
